6997a04b48f92c3b64c1c3e97d75a3f7128d60a7
p-buddy <pmalacho@usc.edu>
2015-11-21 19:29:34 -0800

    Layout of display one, ready to be transformed based on mouse position

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-(function(window) {
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2015, Codrops
+ * http://www.codrops.com
+ */
+;(function(window) {
+
+	'use strict';
 
 	function getMousePos(e) {
 		var posx = 0;
@@ -10,9 +22,9 @@
 		}
 		else if (e.clientX || e.clientY) 	{
 			posx = e.clientX + document.body.scrollLeft
-			+ document.documentElement.scrollLeft;
+				+ document.documentElement.scrollLeft;
 			posy = e.clientY + document.body.scrollTop
-			+ document.documentElement.scrollTop;
+				+ document.documentElement.scrollTop;
 		}
 		return {
 			x : posx,
@@ -20,14 +32,49 @@
 		}
 	}
 
-
-	function Storefront(el) {
+	function Slideshow(el, options) {
+		// if no support for preserve3d then return
+		if( !support.preserve3d ) {
+			return false;
+		}
 
 		this.el = el;
+		this.options = extend( {}, this.options );
+		extend( this.options, options );
 
-		this.displays = $('.display');
+		// the slides
+		this.slides = [].slice.call(this.el.querySelectorAll('.display-container'));
+		// total slides
+		this.slidesTotal = this.slides.length;
+		// the items
+		this.current = 0;
 
 		this._init();
 	}
 
-}
+	Slideshow.prototype.options = {
+		// how much each slider/scene will rotate when the user moves the mouse
+		movement : {
+			rotateX : 5, // a relative rotation of -5deg to 5deg on the x-axis
+			rotateY : 10 // a relative rotation of -10deg to 10deg on the y-axis
+		}
+	};
+
+
+
+	Slideshow.prototype._rotateSlide = function(mousepos) {
+		// transform values
+		var rotX = this.options.movement.rotateX ? 2 * this.options.movement.rotateX / this.slideSizes.height * mousepos.y - this.options.movement.rotateX : 0,
+			rotY = this.options.movement.rotateY ? 2 * this.options.movement.rotateY / this.slideSizes.width * mousepos.x - this.options.movement.rotateY : 0;
+		
+		this.currentScene.style.WebkitTransform = this.currentScene.style.transform = 'rotate3d(1,0,0,' + rotX + 'deg) rotate3d(0,1,0,' + rotY + 'deg)';
+	};
+
+
+	window.Slideshow = Slideshow;
+
+})(window);
+
+
+
+
