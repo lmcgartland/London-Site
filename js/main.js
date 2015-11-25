8bd611e3693eb14e26871aa731db838f606c54f0
Louis Harboe <graphicpeel@gmail.com>
2015-11-25 17:46:37 -0600

    First tests on the building  frame for the displays

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 jQuery(document).ready(function(){
 	/*$('.container').parallax({
 		calibrateX: false,
@@ -19,3 +20,96 @@ jQuery(document).ready(function(){
 		invertY: true
 	});
 });
+=======
+function getMousePos(e) {
+	var posx = 0;
+	var posy = 0;
+	if (!e) var e = window.event;
+	if (e.pageX || e.pageY) 	{
+		posx = e.pageX;
+		posy = e.pageY;
+	}
+	else if (e.clientX || e.clientY) 	{
+		posx = e.clientX + document.body.scrollLeft
+		+ document.documentElement.scrollLeft;
+		posy = e.clientY + document.body.scrollTop
+		+ document.documentElement.scrollTop;
+	}
+	return {
+		x : posx,
+		y : posy
+	}
+}
+
+	var zoomed = false;
+
+function Storefront(el) {
+	this.el = el;
+
+
+	this.displays = $('.display');
+	this.displayItems = $('.item');
+
+	this.currentDisplay = this.displays[0];
+
+	this._init();
+
+
+};
+
+
+Storefront.prototype._init = function() {
+
+
+	var storefrontWidth = ($('.display').width()+100) * $('.display').size() + 100;
+	console.log($('.display').width());
+
+	$('.storefront').width(storefrontWidth);
+
+	console.log($('.storefront').width());
+
+
+};
+
+$( "a.zoom" ).click(function() {
+	var scrollLeft = $('.storefront-stage').scrollLeft() / $('.storefront').width();
+	console.log('bottom ' + $('.storefront-stage').scrollLeft() / $('.storefront').width());
+
+
+
+	if(zoomed) {
+		// $('.storefront').css({transform: 'translateZ(-100px)'});
+		// console.log($('.storefront-stage').scrollLeft());
+		// $('.storefront-wrapper').css({height: '200'});
+		// $('.storefront').css('transform-origin', 'top '+$('.storefront-stage').scrollLeft() / $('.storefront').width());
+				// $('.storefront').css({transform: 'scale(1,1)'});
+
+		TweenMax.to(".storefront", 0.4, {
+            z: 0,
+            y: 0,
+			ease: Quad.easeInOut
+        });
+
+		zoomed = false;
+	}
+	else {
+		// $('.storefront').css({transform: 'translateZ(0px)'});
+		// $('.storefront-wrapper').css({height: '600'});
+		// $('.storefront').css('transform-origin', 'top '+$('.storefront-stage').scrollLeft() / $('.storefront').width());
+		// 		$('.storefront').css({transform: 'scale(0.5,0.5)'});
+
+
+		TweenMax.to(".storefront", 0.4, {
+           z: -100,
+           y: 200,
+			ease: Quad.easeInOut
+        });
+
+		zoomed = true;
+
+	}
+
+});
+
+
+>>>>>>> Stashed changes
