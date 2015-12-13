765d5743dcc8f2a1bb63e8a07775232e4fbc4e9d
Nathan Wallace <techietotoro@gmail.com>
2015-12-13 13:57:21 -0800

    Clean up script

--- js/main.js
+++ js/main.js
@@ -1,46 +1,29 @@
 jQuery(document).ready(function(){
-	/*$('.container').parallax({
-		calibrateX: false,
-		calibrateY: true,
-		invertX: false,
-		invertY: true,
-		limitX: false,
-		limitY: 10,
-		scalarX: 2,
-		scalarY: 8,
-		frictionX: 0.2,
-		frictionY: 0.8,
-		originX: 0.0,
-		originY: 1.0
-	});*/
 	$('#scene').parallax({
 		relativeInput: true,
 		clipRelativeInput: true,
 		invertY: true
 	});
+
+	$("#display-1").click(function(){
+		$("#ov-1").animate({"margin-top": "0vh"});
+		$("#oc-1").animate({"visibility": "visible"});
+	});
+	$("#display-2").click(function(){
+		$("#ov-1").animate({"margin-top": "0vh"});
+		$("#oc-2").animate({"visibility": "visible"});
+	});
+	$("#ov-1").click(function(){
+		$("#ov-1").animate({"margin-top": "100vh"});
+		$(".overlay-content").animate({"visibility": "hidden"});
+	});
+
+	(function() {
+		var storefront = new Storefront($('.storefront'));
+	})();
 });
 
-function getMousePos(e) {
-	var posx = 0;
-	var posy = 0;
-	if (!e) var e = window.event;
-	if (e.pageX || e.pageY) 	{
-		posx = e.pageX;
-		posy = e.pageY;
-	}
-	else if (e.clientX || e.clientY) 	{
-		posx = e.clientX + document.body.scrollLeft
-		+ document.documentElement.scrollLeft;
-		posy = e.clientY + document.body.scrollTop
-		+ document.documentElement.scrollTop;
-	}
-	return {
-		x : posx,
-		y : posy
-	}
-}
-
-	var zoomed = false;
+var zoomed = false;
 
 function Storefront(el) {
 	this.el = el;
@@ -66,8 +49,6 @@ Storefront.prototype._init = function() {
 	$('.storefront').width(storefrontWidth);
 	$('.storefront').offset({ top: -100 });
 	console.log($('.storefront').width());
-
-
 };
 
 $( "a.zoom" ).click(function() {
@@ -83,26 +64,26 @@ $( "a.zoom" ).click(function() {
 		// $('.storefront').css('transform-origin', 'top '+$('.storefront-stage').scrollLeft() / $('.storefront').width());
 				// $('.storefront').css({transform: 'scale(1,1)'});
 
-		TweenMax.to(".storefront", 0.4, {
-            z: 0,
-            y: 0,
-			ease: Quad.easeInOut
-        });
+				TweenMax.to(".storefront", 0.1, {
+					z: 0,
+					y: 0,
+					ease: Linear
+				});
 
-		zoomed = false;
-	}
-	else {
+				zoomed = false;
+			}
+			else {
 		// $('.storefront').css({transform: 'translateZ(0px)'});
 		// $('.storefront-wrapper').css({height: '600'});
 		// $('.storefront').css('transform-origin', 'top '+$('.storefront-stage').scrollLeft() / $('.storefront').width());
 		// 		$('.storefront').css({transform: 'scale(0.5,0.5)'});
 
 
-		TweenMax.to(".storefront", 0.4, {
-           z: -100,
-           y: 0,
-			ease: Quad.easeInOut
-        });
+		TweenMax.to(".storefront", 0.1, {
+			z: -100,
+			y: 0,
+			ease: Linear
+		});
 
 		zoomed = true;
 
