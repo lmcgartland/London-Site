fb447bdda5651eb28b5a78ab48f4f28f9aef8e18
Nathan Wallace <techietotoro@gmail.com>
2015-12-13 23:56:56 -0800

    Add travel window

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
 	var data;
 
-	$.getJSON('data/process_window_data.json', function(response){
+	$.getJSON('data/travel_window_data.json', function(response){
 		data = response;
 	})
 	.success(function() { 
