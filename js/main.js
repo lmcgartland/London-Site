888c7fb181785c313acfa6f549b1b16379105949
Nathan Wallace <techietotoro@gmail.com>
2015-12-13 20:52:25 -0800

    Add culture window

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 jQuery(document).ready(function(){
 	var data;
 
-	$.getJSON('data/media_window_data.json', function(response){
+	$.getJSON('data/culture_window_data.json', function(response){
 		data = response;
 	})
 	.success(function() { 
 		var template = $.templates("#layerTemplate");
 		var htmlOutput = template.render(data);
-		$("#scene").html(htmlOutput).parallax({
-			relativeInput: true,
-			clipRelativeInput: true,
-			invertY: true
-		});
+		$("#scene").html(htmlOutput);
 	});
 });
